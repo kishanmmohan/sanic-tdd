60fc07d604b4730f4323761b09616d845dbfdf5b
kishan-radius <kishan.mohan@radius.ai>
2024-08-22 11:23:16 +0530

    configs update

--- app/config/environments/base.py
+++ app/config/environments/base.py
@@ -1,5 +1,6 @@
 import inspect
 import os
+import sys
 
 
 class BaseConfig:
@@ -8,7 +9,60 @@ class BaseConfig:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     HOST = "0.0.0.0"
     PORT = 8000
-    WORKERS_COUNT = 4
+    WORKERS_COUNT = 1
+
+    LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'loggers': {
+        'sanic.root': {'level': 'INFO', 'handlers': ['console']},
+        'sanic.error': {
+            'level': 'INFO',
+            'handlers': ['error_console'],
+            'propagate': True,
+            'qualname': 'sanic.error'
+        },
+        'sanic.access': {
+            'level': 'INFO',
+            'handlers': ['access_console'],
+            'propagate': True,
+            'qualname': 'sanic.access'
+        },
+        'sanic.server': {
+            'level': 'INFO',
+            'handlers': ['console'],
+            'propagate': True,
+            'qualname': 'sanic.server'
+        },
+        'sanic.websockets': {
+            'level': 'INFO',
+            'handlers': ['console'],
+            'propagate': True,
+            'qualname': 'sanic.websockets'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'generic',
+            'stream': sys.stdout
+        },
+        'error_console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'generic',
+            'stream': sys.stderr
+        },
+        'access_console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'access',
+            'stream': sys.stdout
+        }
+    },
+    'formatters': {
+        'generic': {'class': 'sanic.logging.formatter.AutoFormatter'},
+        'access': {'class': 'sanic.logging.formatter.AutoAccessFormatter'}
+    }
+}
 
     @classmethod
     def get_vars(cls):
--- main.py
+++ main.py
@@ -1,71 +1,16 @@
-import sys
 
 import sentry_sdk
 from sanic import Sanic, response
-from sanic.log import logger
 from sanic_ext import Extend
 from sentry_sdk.integrations.asyncio import AsyncioIntegration
 
-LOGGING_CONFIG = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'loggers': {
-        'sanic.root': {'level': 'INFO', 'handlers': ['console']},
-        'sanic.error': {
-            'level': 'INFO',
-            'handlers': ['error_console'],
-            'propagate': True,
-            'qualname': 'sanic.error'
-        },
-        'sanic.access': {
-            'level': 'INFO',
-            'handlers': ['access_console'],
-            'propagate': True,
-            'qualname': 'sanic.access'
-        },
-        'sanic.server': {
-            'level': 'INFO',
-            'handlers': ['console'],
-            'propagate': True,
-            'qualname': 'sanic.server'
-        },
-        'sanic.websockets': {
-            'level': 'INFO',
-            'handlers': ['console'],
-            'propagate': True,
-            'qualname': 'sanic.websockets'
-        }
-    },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'generic',
-            'stream': sys.stdout
-        },
-        'error_console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'generic',
-            'stream': sys.stderr
-        },
-        'access_console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'access',
-            'stream': sys.stdout
-        }
-    },
-    'formatters': {
-        'generic': {'class': 'sanic.logging.formatter.AutoFormatter'},
-        'access': {'class': 'sanic.logging.formatter.AutoAccessFormatter'}
-    }
-}
 
 def create_app(configs) -> Sanic:
-    app = Sanic("products-app", log_config=LOGGING_CONFIG)
+    app = Sanic("products-app", log_config=configs.LOGGING_CONFIG)
     app.config.update(configs)
-    print(app.config.ENVIRONMENT)
     Extend(app)
 
-    if app.config["ENVIRONMENT"] != "development":
+    if app.config.ENVIRONMENT != "development":
         sentry_sdk.init(
             dsn="sentry_dsn",
             # Set traces_sample_rate to 1.0 to capture 100%
@@ -80,7 +25,6 @@ def create_app(configs) -> Sanic:
 
     @app.route('/health')
     async def health_check(request):
-        logger.info("Testing logging impl")
-        return response.json({"status": "ok"})
+        return response.json({"status": "ok", "environment":app.config.ENVIRONMENT})
 
     return app
